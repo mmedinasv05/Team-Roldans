f3b7c12c963a4153dccc8add88b530b6c8d4ce80
Manuel Chavez <chavezofficial2015@gmail.com>
2021-11-22 07:08:29 -0600

    22/11/2021

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ export const releases = [
         title: "COMUNICADO: Cancelación de envío por paquetería - ARG.",
         urlBeedoo: "https://atentoop10.beedoo.io/feed/282666/post-cancelacion-de-envio-por-paqueteria-arg",
         image: "https://s3.amazonaws.com/beedoo-user-us/atentoop10/gaby.rivera/post/img/aff8cc1c63f4af983bcd159e9da3c26f.png",
-        prhase: "H"
+        prhase: "V"
     },
     {
         id: 152,
